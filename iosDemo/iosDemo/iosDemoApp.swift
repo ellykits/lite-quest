aeb89a5fb358031ec3e8ef24dada04ae82b405f6
Elly Kitoto <ellynerdstone@gmail.com>
2025-12-02 15:53:19 +0300

    Add IOs demo
    Signed-off-by: Elly Kitoto <ellynerdstone@gmail.com>

---
+++ iosDemo/iosDemo/iosDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosDemoApp.swift
+//  iosDemo
+//
+//  Created by Elly Kitoto on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosDemo/iosDemoTests/iosDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosDemoTests.swift
+//  iosDemoTests
+//
+//  Created by Elly Kitoto on 30/11/2025.
+//
+
+import Testing
+@testable import iosDemo
+
+struct iosDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
